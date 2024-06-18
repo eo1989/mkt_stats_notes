8eb381e1e7e0e7a7f6d82778392da7843b9f7fee
eo1989 <7108365+eo1989@users.noreply.github.com>
2024-06-18 12:35:47 -0400

    more notes, mostly from fluent python book ch11

--- bokeh_hv_ex.py
+++ bokeh_hv_ex.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import hvplot.streamz
+import hvplot.pandas
 import holoviews as hv
-
 from streamz import Stream
 from streamz.dataframe import DataFrame
 
@@ -14,15 +14,15 @@ stream = Stream()
 index = pd.DatetimeIndex([])
 ex = pd.DataFrame({"x": [], "y": [], "z": []}, columns=["x", "y", "z"], index=[])
 
-df = DataFrame(stream, example=ex)
-cumulative = df.cumsum()[["x", "z"]]
+_df = DataFrame(stream, example=ex)
+cumulative = _df.cumsum()[["x", "z"]]
 
 # Declaring the plots to be used
 line = cumulative.hvplot.line(width=400)
 scatter = cumulative.hvplot.scatter(width=400)
-bars = df.groupby("y").sum().hvplot.bar(width=400)
-box = df.hvplot.box(width=400)
-kde = df.x.hvplot.kde(width=400)
+bars = _df.groupby("y").sum().hvplot.bar(width=400)
+box = _df.hvplot.box(width=400)
+kde = _df.x.hvplot.kde(width=400)
 
 
 # composing the plots
@@ -34,7 +34,7 @@ def emit():
     now = pd.datetime.now()
     delta = np.timedelta64(500, "ms")
     index = pd.date_range(np.datetime64(now) - delta, now, freq="100ms")
-    df = pd.DataFrame(
+    _df = pd.DataFrame(
         {
             "x": np.random.randn(len(index)),
             "y": np.random.randint(0, 10, len(index)),
@@ -43,7 +43,7 @@ def emit():
         columns=["x", "y", "z"],
         index=index,
     )
-    stream.emit(df)
+    stream.emit(_df)
 
 
 # render the layout w/ Bokeh server Document and attach the callback function
